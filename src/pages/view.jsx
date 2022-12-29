f12da92be387ac82799ea3e178f0754580826718
Benjamin <benjamin.bastidas@impact.cl>
2022-12-29 20:20:21 -0300

    imagenes webp

--- src/pages/view.jsx
+++ src/pages/view.jsx
@@ -9,7 +9,7 @@ import useImages from "../hooks/useImages";
 export default function View() {
   const { name } = useParams();
   const containerRef = useRef(null);
-  const loaderRef = useRef(null);
+  const loaderRef = useRef(null)
   const [project, setProject] = useState(null);
   const { imagesLoaded, imagesLoadError, setImages } = useImages();
   const [loading, setLoading] = useState(true);
@@ -19,110 +19,111 @@ export default function View() {
       const project = proyectos.find((project) => project.name === name);
       if (project) {
         setProject(project);
-        const imgs = Array.from(containerRef.current.querySelectorAll("img"));
-        setImages(imgs);
+        
       }
     }
   }, []);
   useEffect(() => {
     if (imagesLoaded) {
-      console.log(loaderRef);
-      loaderRef.current.classList.add("active");
+      loaderRef.current.classList.add("active")
       setTimeout(() => {
         setLoading(false);
-        document.body.classList.remove("loading");
+      document.body.classList.remove("loading");
       }, 300);
     }
   }, [imagesLoaded]);
-  useEffect(() => {}, [location]);
+  useEffect(() => {
+    const imgs = Array.from(containerRef.current.querySelectorAll("img"));
+    setImages(imgs);
+  }, [project]);
   return (
     <div className="app">
       {loading ? <Loader refLoader={loaderRef}></Loader> : null}
-
-      <main className="container container-view" ref={containerRef}>
-        <section className="section-main">
-          <div className="section-main__header">
-            <div className="header__information">
-              <h6 className="header__tipo">{project?.tipo}</h6>
-              <h1 className="header__title section-title">{project?.name}</h1>
+      
+        <main className="container container-view" ref={containerRef}>
+          <section className="section-main">
+            <div className="section-main__header">
+              <div className="header__information">
+                <h6 className="header__tipo">{project?.tipo}</h6>
+                <h1 className="header__title section-title">{project?.name}</h1>
+              </div>
+              <LinkAbout link={project?.link} icon="github" />
             </div>
-            <LinkAbout link={project?.link} icon="github" />
-          </div>
-          <div className="section-main__content">
-            <div className="main__image section__image">
-              <picture>
-                <source
-                  type="image/webp"
-                  srcSet={`/img/${project?.imageMain}.webp`}
-                />
-                <img
-                  src={`/img/${project?.image}/${project?.imageMain}.png`}
+            <div className="section-main__content">
+              <div className="main__image section__image">
+                {
+                  project?.image && (
+                    <img
+                  src={`/img/${project?.image}/${project?.imageMain}.webp`}
                   alt=""
                 />
-              </picture>
-            </div>
-            <div className="main__information">
-              <div className="main__tipo">{project?.puesto}</div>
-              <div className="main__buttonwrapper">
-                <a
-                  className={`btn main__button ${
-                    project?.linkPagina ? "" : "block"
-                  }`}
-                  href={project?.linkPagina}
-                  target="_blank">
-                  Ir a la pagina
-                </a>
+                  )
+                }
               </div>
-            </div>
-          </div>
-        </section>
-        {project?.sections?.map((section) => (
-          <section className="section">
-            <div className="section__information">
-              <h4 className="section-title">{section.title}</h4>
-              <div className="section__description">{section?.description}</div>
-            </div>
-            <div className="wrapper-image">
-              <div
-                className={`section__image ${
-                  section?.image?.length > 1 ? "no-shadow" : ""
-                }`}>
-                {section?.image?.map((image) => (
-                  <img
-                    src={`/img/${project?.image}/${image.image}.png`}
-                    alt=""
-                  />
-                ))}
+              <div className="main__information">
+                <div className="main__tipo">{project?.puesto}</div>
+                <div className="main__buttonwrapper">
+                  <a
+                    className={`btn main__button ${
+                      project?.linkPagina ? "" : "block"
+                    }`}
+                    href={project?.linkPagina}
+                    target="_blank">
+                    Ir a la pagina
+                  </a>
+                </div>
               </div>
             </div>
           </section>
-        ))}
-        <div className="project-used">
-          <div className="tools-container">
-            <h4 className="section-title">Tools used</h4>
-            <div className="tools">
-              {project?.languages?.map((lang) => (
-                <div className="tools__item">
-                  <img src={`/img/${lang}.png`} alt="" />
+          {project?.sections?.map((section) => (
+            <section className="section">
+              <div className="section__information">
+                <h4 className="section-title">{section.title}</h4>
+                <div className="section__description">
+                  {section?.description}
                 </div>
-              ))}
+              </div>
+              <div className="wrapper-image">
+                <div
+                  className={`section__image ${
+                    section?.image?.length > 1 ? "no-shadow" : ""
+                  }`}>
+                  {section?.image?.map((image) => (
+                    <img
+                      src={`/img/${project?.image}/${image.image}.webp`}
+                      alt=""
+                    />
+                  ))}
+                </div>
+              </div>
+            </section>
+          ))}
+          <div className="project-used">
+            <div className="tools-container">
+              <h4 className="section-title">Tools used</h4>
+              <div className="tools">
+                {project?.languages?.map((lang) => (
+                  <div className="tools__item">
+                    <img src={`/img/${lang}.webp`} alt="" />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="palette-container">
+              <h4 className="section-title">Palette</h4>
+              <div className="palette">
+                {project?.palette?.map((palette) => (
+                  <div className="palette__item">
+                    <div
+                      className="palette__item__background"
+                      style={{ backgroundColor: palette.color }}></div>
+                    <h6>{palette.color}</h6>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-          <div className="palette-container">
-            <h4 className="section-title">Palette</h4>
-            <div className="palette">
-              {project?.palette?.map((palette) => (
-                <div className="palette__item">
-                  <div
-                    className="palette__item__background"
-                    style={{ backgroundColor: palette.color }}></div>
-                  <h6>{palette.color}</h6>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </main>
+        </main>
     </div>
   );
 }
