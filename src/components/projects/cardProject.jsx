ae4f182dd7af209ab6d91c361e62c5a260f0debe
Benjamin <benjamin.bastidas@impact.cl>
2023-01-09 11:33:39 -0300

    cambios en background por position

--- src/components/projects/cardProject.jsx
+++ src/components/projects/cardProject.jsx
@@ -25,7 +25,7 @@ export default function CardProject({ item }) {
                     alt=""
                   />
                 ) : null}
-                Ver más 
+                Ver más
               </div>
             </div>
             <div className="card__footer">
@@ -42,11 +42,11 @@ export default function CardProject({ item }) {
       ) : (
         <div className={`card ${item.completed === true ? "" : "developing"} `}>
           <div className="card__image">
-          <img
-                src={`/img/${item?.image}.webp`}
-                alt="project-image"
-                className="card__image__principal"
-              />
+            <img
+              src={`/img/${item?.image}.webp`}
+              alt="project-image"
+              className="card__image__principal"
+            />
             {item?.completed ? (
               <Link to={`project/${item?.name}`}>
                 <div className="placeholder">
