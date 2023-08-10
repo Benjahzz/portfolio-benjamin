86776ce3a01af759209d028236c826a1d9c6f47f
Benjahhz <ben.ja07@live.cl>
2023-08-10 16:20:09 -0400

    Changes in loading screen

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -8,38 +8,38 @@ export default function Loading() {
         width="78"
         height="127"
         viewBox="0 0 78 127"
-        fill="none"
+        className="text-black dark:text-white"
         xmlns="http://www.w3.org/2000/svg">
         <path
           d="M4.19946 94.3262L14.8596 105.858V126.307L0 105.858L4.19946 94.3262Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M19.3823 106.737L68.4838 91.7421L77.5288 104.663L19.3823 126.63V106.737Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M60.7306 94.762L77.5284 84.6353V104.664L60.7306 94.762Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M36.8259 57.5005L77.5284 84.6355L65.8991 94.9727L36.8259 78.8209V57.5005Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M57.5002 40.3797L70.4217 32.3038V73.6523L57.5002 65.8995V40.3797Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M24.5507 -6.10352e-05L70.0987 27.781L55.2391 38.4412L24.5507 21.3203V-6.10352e-05Z"
-          fill="white"
+          fill="currentColor"
         />
         <path
           d="M10.6602 22.6126L31.0114 33.5958V100.787L10.6602 89.158V22.6126Z"
-          fill="white"
+          fill="currentColor"
         />
       </svg>
-      Loading...
+      <p className="text-black dark:text-white">Loading...</p>
     </div>
 
   )
