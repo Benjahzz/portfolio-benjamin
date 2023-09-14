0c1d78d4c9cd3e16f7ea07fbe4378fb2dc5cee60
Benjahhz <ben.ja07@live.cl>
2023-09-14 16:26:27 -0300

    change meta

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Benjamin Bastidas | Desarrollador web',
-  description: 'Soy benjamín bastidas, este es mi portafolio enfocado en mis desarrollos web y sobre mi.',
+  description: 'Soy benjamín bastidas, este es mi portafolio.',
   openGraph:{
     type:'website',
     locale:'es_ES',
